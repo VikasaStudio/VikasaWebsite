128258ca1ae5b0d85a7b5d27c7bf58c0f92d7afa
= <=>
2021-06-25 01:09:03 +0530

    added compressed imgs

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ window.scrollVal = 0;
 
 var updateScrollProperty = function () {
   window.requestAnimationFrame(updateScrollProperty);
+  try{
   window.scrollVal =
     window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
   document.documentElement.style.setProperty("--scroll", window.scrollVal);
+  }catch(err){}
 };
 
 window.requestAnimationFrame(updateScrollProperty);
