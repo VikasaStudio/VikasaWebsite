c251599a3bfe44718f0437308287f0af69852dda
= <=>
2021-06-27 02:29:18 +0530

    added content

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ var updateScrollProperty = function () {
   try{
   window.scrollVal =
     window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
+    
   document.documentElement.style.setProperty("--scroll", window.scrollVal);
   }catch(err){}
 };
