98e5682c18e46fb41dfe8b7a9f16eecb6089b719
= <=>
2021-06-27 18:59:28 +0530

    gsap integrated

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
-window.scrollVal = 0;
 
-var updateScrollProperty = function () {
-  window.requestAnimationFrame(updateScrollProperty);
-  try{
-  window.scrollVal =
-    window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
-    
-  document.documentElement.style.setProperty("--scroll", window.scrollVal);
-  }catch(err){}
-};
+gsap.registerPlugin(ScrollTrigger);
 
-window.requestAnimationFrame(updateScrollProperty);
-updateScrollProperty();
+var animTimeline = gsap.timeline();
+//page1
+animTimeline.fromTo("#page-landing", {y: 0}, {y:'-90vh'});
+animTimeline.fromTo('#page-goOnline', {y:'100vh'}, {y:'-200vh'});
+animTimeline.fromTo('#page-expectMore', {y:'100vh'},{y:'-150vh'});
+animTimeline.fromTo('#page-expectMore', {opacity:1},{opacity:0} );
+animTimeline.fromTo('#page-landing', {opacity:1},{opacity:0});
+animTimeline.fromTo('#page-showcase', {y:'100vh'}, { y:'-50vh'});
+animTimeline.fromTo('#page-contactUs', {y:'100vh'}, { y:'0vh'});
+
+ScrollTrigger.create({
+  animation: animTimeline,
+  scrub:true,
+})
