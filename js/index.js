01026ae97743da7dd5f2e7730e0ded4f17b7fca5
= <=>
2021-06-28 15:38:54 +0530

    updated design of payment page

--- js/index.js
+++ js/index.js
@@ -7,11 +7,13 @@ var animTimeline = gsap.timeline({
   }
 });
 
+var pageHeight = {};
+
 //page1
 animTimeline.fromTo("#page-landing", 10,{y: 0}, {y:'-90vh'});
-animTimeline.fromTo('#page-goOnline', 10, {y:'100vh'}, {y:'-250vh'},"-=6");
-animTimeline.fromTo('#page-expectMore', 10,{y:'100vh'},{y:'-150vh'},"-=5.5");
-animTimeline.fromTo('#page-expectMore', {opacity:1},{opacity:0});
+animTimeline.fromTo('#page-goOnline', 10, {y:'100vh'}, {y:'-280vh'},"-=6");
+animTimeline.fromTo('#page-expectMore', 10,{y:'100vh'},{y:'-150vh'},"-=4.5");
+animTimeline.fromTo('#page-expectMore', {opacity:1},{opacity:0}, "-=3");
 animTimeline.fromTo('#page-landing', {opacity:1},{opacity:0},"-=7");
 animTimeline.fromTo('#page-showcase', 10, {y:'100vh'}, { y:'-50vh'}, "-=6");
 animTimeline.fromTo('#page-contactUs', 10, {y:'100vh'}, { y:'0vh'});
