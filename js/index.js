29b2a22d12c104fb84cd199399ce976a7e15ec44
= <=>
2021-06-30 03:53:59 +0530

    scrub decrase in mobile

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ ScrollTrigger.matchMedia({
   "(min-width:320px) and (max-width:666px)": function(){
     var animTimeline = gsap.timeline({
       scrollTrigger:{
-        scrub:0.5
+        scrub:0.1
       }
     });
     animTimeline.fromTo("#page-landing", 10,{y: 0}, {y:'-90vh'});
